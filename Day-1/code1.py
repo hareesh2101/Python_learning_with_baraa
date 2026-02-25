d33ae716c55e198bd98cdeb3977d4ce9f6b55dee
harish <harish@harishs-MacBook-Air.local>
2026-02-25 19:20:39 +0530

    day-1 learning python

---
+++ Day-1/code1.py
@@ -0,0 +1,22 @@
+# topics covered --- variable , print_fu, input_fu, Comments
+
+# variables
+x = 10
+y = 19
+name = 'harish'
+
+# print()
+print(f'vales are {x},{y},{name}')
+
+
+# input()
+user_name = input('enter your name ')
+print(f'hello {user_name} Welocme..')
+
+
+# commnets - this are non functional lines of codes used for describing the code steps/ implimentation like line number -1, 3,8 12
+
+# hardcoded
+
+
+# Dynamic
